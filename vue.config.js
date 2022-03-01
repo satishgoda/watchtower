9120d83d3ce26d5d7daa9cabda1e17131a056cb8
Francesco Siddi <francesco.siddi@gmail.com>
2022-03-01 19:36:34 +0100

    Define publicPath as ''
    This allows for a build to be deployed on any url path (root or
    subdirectory).

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/watchtower/'
-    : '/',
+  publicPath: '',
   devServer: {
     host: 'localhost',
     proxy: {
